ec7da3f28426b2f9c61da719a2533e79b0a48284
fasana-corrado <72620700+fasana-corrado@users.noreply.github.com>
2022-06-30 10:07:26 +0200

    New configuration

--- repos/pytorch-CycleGAN-and-pix2pix/models/networks.py
+++ repos/pytorch-CycleGAN-and-pix2pix/models/networks.py
@@ -366,8 +366,7 @@ class ResnetGenerator(nn.Module):
                       nn.ReLU(True)]
         model += [nn.ReflectionPad2d(3)]
         model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
-        model += [norm_layer(output_nc),
-                 nn.ReLU(True)]
+        model += [nn.Tanh()]
 
         self.model = nn.Sequential(*model)
 
@@ -579,7 +578,7 @@ class NLayerDiscriminator(nn.Module):
             nn.ReLU(True)
         ]
 
-        sequence += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw), norm_layer(1), nn.ReLU(True)]  # output 1 channel prediction map
+        sequence += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]  # output 1 channel prediction map
         self.model = nn.Sequential(*sequence)
 
     def forward(self, input):
