8e7708563e7324c9e3bf9393b93730526e708cd4
PasiniSamuele <10617905@polimi.it>
2022-06-30 11:28:29 +0200

    ecycle gan

--- repos/pytorch-CycleGAN-and-pix2pix/models/networks.py
+++ repos/pytorch-CycleGAN-and-pix2pix/models/networks.py
@@ -154,6 +154,8 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
         net = UnetGenerator(input_nc, output_nc, 7, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     elif netG == 'unet_256':
         net = UnetGenerator(input_nc, output_nc, 8, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
+    elif netG == "RDNB" :
+        net = RDNBGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=9)
     else:
         raise NotImplementedError('Generator model name [%s] is not recognized' % netG)
     return init_net(net, init_type, init_gain, gpu_ids)
@@ -311,6 +313,63 @@ def cal_gradient_penalty(netD, real_data, fake_data, device, type='mixed', const
     else:
         return 0.0, None
 
+class RDNBGenerator(nn.Module):
+
+    def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=6, padding_type='zero', num_dense_layers = 3, num_dense_subblocks = 4):
+        """Construct a Resnet-based generator
+
+        Parameters:
+            input_nc (int)      -- the number of channels in input images
+            output_nc (int)     -- the number of channels in output images
+            ngf (int)           -- the number of filters in the last conv layer
+            norm_layer          -- normalization layer
+            use_dropout (bool)  -- if use dropout layers
+            n_blocks (int)      -- the number of ResNet blocks
+            padding_type (str)  -- the name of padding layer in conv layers: reflect | replicate | zero
+        """
+
+        p=1
+        assert(n_blocks >= 0)
+        super(RDNBGenerator, self).__init__()
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
+                 norm_layer(ngf),
+                 nn.ReLU(True)]
+
+        n_downsampling = 2
+        for i in range(n_downsampling):  # add downsampling layers
+            mult = 2 ** i
+            model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=p, bias=use_bias),
+                      norm_layer(ngf * mult * 2),
+                      nn.ReLU(True)]
+
+        mult = 2 ** n_downsampling
+        for i in range(n_blocks):       # add ResNet blocks
+
+            model += [RDNB(ngf * mult, padding_type=padding_type, norm_layer=norm_layer, use_dropout=use_dropout, use_bias=use_bias)]
+
+        for i in range(n_downsampling):  # add upsampling layers
+            mult = 2 ** (n_downsampling - i)
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+                                         kernel_size=3, stride=2,
+                                         padding=p, output_padding=p,
+                                         bias=use_bias),
+                      norm_layer(int(ngf * mult / 2)),
+                      nn.ReLU(True)]
+        model += [nn.ReflectionPad2d(3)]
+        model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        model += [nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, input):
+        """Standard forward"""
+        return self.model(input)
 
 class ResnetGenerator(nn.Module):
     """Resnet-based generator that consists of Resnet blocks between a few downsampling/upsampling operations.
@@ -374,6 +433,80 @@ class ResnetGenerator(nn.Module):
         """Standard forward"""
         return self.model(input)
 
+class DenseBlock(nn.Module):
+    """Define a Dense Block used in RDNB"""
+
+    def __init__(self, dim, padding_type, norm_layer, num_subblocks = 4):
+        """Initialize the Dense block """
+        super(DenseBlock, self).__init__()
+        self.sub_blocks, self.conv_final_block = self.build_dense_block(dim, padding_type, norm_layer, num_subblocks)
+
+    def build_dense_block(self, dim, padding_type, norm_layer, num_subblocks):
+        """Construct a dense block.
+
+        Parameters:
+            dim (int)           -- the number of channels in the conv layer.
+            padding_type (str)  -- the name of padding layer: reflect | replicate | zero
+            norm_layer          -- normalization layer
+
+        Returns a conv block (with a conv layer, a normalization layer, and a non-linearity layer (ReLU))
+        """
+        conv_block = []
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(1)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(1)]
+        elif padding_type == 'zero':
+            p = 1
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+
+        sub_blocks = [self.build_subdense_block(dim, p, norm_layer) for i in range (num_subblocks)]
+
+        conv_final_block = [nn.Conv2d(dim, dim, kernel_size=3, padding=p), norm_layer(dim)]
+
+        return sub_blocks, conv_final_block
+
+    def build_subdense_block(self, dim, padding, norm_layer):
+        block = [
+            nn.Conv2d(dim, dim, kernel_size = 3, padding = padding),
+            norm_layer(dim),
+            nn.ReLU(True)
+        ]
+        return nn.Sequential(*block)
+
+    def forward(self, x):
+        block_outputs = [x]
+
+        for block in self.sub_blocks:
+            inputs = torch.sum(block_outputs)
+            blocks_outputs += block(inputs)
+
+        out = self.conv_final_block(block_outputs) 
+        return out
+
+class RDNB(nn.Module):
+    """Define a RDNB block"""
+    def __init__(self, dim, padding_type, norm_layer, use_dropout, use_bias, num_dense_layers = 3, num_dense_subblocks = 4):
+        """Initialize the Resnet block
+
+        A resnet block is a conv block with skip connections
+        We construct a conv block with build_conv_block function,
+        and implement skip connections in <forward> function.
+        Original Resnet paper: https://arxiv.org/pdf/1512.03385.pdf
+        """
+        super(RDNB, self).__init__()
+        self.rdnb_block = self.build_rdnb_block(dim, padding_type, norm_layer, use_dropout, use_bias, num_dense_layers, num_dense_subblocks)
+
+    def build_rdnb_block(self, dim, padding_type, norm_layer, use_dropout, use_bias, num_dense_layers,num_dense_subblocks):
+        return DenseBlock(dim, padding_type, norm_layer, num_dense_subblocks)
+
+    def forward(self, x):
+        """Forward function"""
+        out = x + self.rdnb_block(x)  
+        return out
+    
 
 class ResnetBlock(nn.Module):
     """Define a Resnet block"""
