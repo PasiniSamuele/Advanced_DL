c957963694c47313698b20d80dc27666f7f1f9dd
fasana-corrado <72620700+fasana-corrado@users.noreply.github.com>
2022-09-14 11:15:14 +0200

    Alternate training

--- code/cycle_gan/models/ecycle_gan_model.py
+++ code/cycle_gan/models/ecycle_gan_model.py
@@ -85,8 +85,8 @@ class ECycleGANModel(BaseModel):
                                         not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids, 
                                         opt.num_dense_layers, opt.num_dense_subblocks, opt.residual_scaling)
 
-        self.D_threshold = opt.discriminator_threshold
         if self.isTrain:  # define discriminators
+            self.D_threshold = opt.discriminator_threshold
             self.netD_A = networks.define_D(opt.output_nc, opt.ndf, opt.netD,
                                             opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
             self.netD_B = networks.define_D(opt.input_nc, opt.ndf, opt.netD,
@@ -206,30 +206,28 @@ class ECycleGANModel(BaseModel):
         self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B
         self.loss_G.backward()
 
-    def optimize_parameters(self, iteration, tot_iterations):
+    def optimize_parameters(self, iteration, tot_iterations, epoch):
         """Calculate losses, gradients, and update network weights; called in every training iteration"""
         # forward
         self.forward()      # compute fake images and reconstruction images.
         # G_A and G_B
-        self.set_requires_grad([self.netD_A, self.netD_B], False)  # Ds require no gradients when optimizing Gs
-        self.optimizer_G.zero_grad()  # set G_A and G_B's gradients to zero
-        self.backward_G()             # calculate gradients for G_A and G_B
-        self.optimizer_G.step()       # update G_A and G_B's weights
-        # D_A and D_B
-        
-        # self.backward_D_A()      # calculate gradients for D_A
-        # self.backward_D_B()      # calculate graidents for D_B
-        # self.optimizer_D.step()  # update D_A and D_B's weights
-        self.set_requires_grad([self.netD_A, self.netD_B], True)
-        self.optimizer_D.zero_grad()   # set D_A and D_B's gradients to zero
-        self.backward_D_A()      # calculate gradients for D_A
-        self.backward_D_B()      # calculate graidents for D_B
+        if epoch < 3 or iteration % 10 != 0:
+            self.set_requires_grad([self.netD_A, self.netD_B], False)  # Ds require no gradients when optimizing Gs
+            self.optimizer_G.zero_grad()  # set G_A and G_B's gradients to zero
+            self.backward_G()             # calculate gradients for G_A and G_B
+            self.optimizer_G.step()       # update G_A and G_B's weights
+        if epoch < 3 or iteration % 10 == 0:
+            self.set_requires_grad([self.netD_A, self.netD_B], True)
+            self.optimizer_D.zero_grad()   # set D_A and D_B's gradients to zero
+            self.backward_D_A()      # calculate gradients for D_A
+            self.backward_D_B()      # calculate graidents for D_B
+            self.optimizer_D.step()
 
         #th_A = self.get_th_discriminator_A()
         #th_B = self.get_th_discriminator_B()
         #print(th_A)
 
-        if iteration > tot_iterations/2:
+        '''if iteration > tot_iterations/2:
 
             if self.loss_D_A >= self.D_threshold:
                 #self.set_requires_grad([self.netD_A], True)
@@ -243,4 +241,4 @@ class ECycleGANModel(BaseModel):
                 self.backward_D_B()
                 self.optimizer_D.step()  # update D_A and D_B's weights
         else:
-            self.optimizer_D.step()  # update D_A and D_B's weights
+            self.optimizer_D.step()  # update D_A and D_B's weights'''
